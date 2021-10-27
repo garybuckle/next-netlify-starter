ac862d9a0ab3da1e26197b35b55efd51d38fe2ef
Gary Buckle <buckle.g@mac.com>
2021-10-27 09:58:58 +0100

    Added Bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 // import '@styles/globals.css'
 // import 'tailwindcss/tailwind.css'
+// try bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
