43b34801f1e4927af34c3444496458386c0db899
Gary Buckle <buckle.g@mac.com>
2021-10-27 09:46:27 +0100

    Tailwind init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@styles/globals.css'
-
+import 'tailwindcss/tailwind.css'
+import '../styles/globals.css'
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
